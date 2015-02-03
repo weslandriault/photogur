e347f72f623fe372fa2d88c6010b3ddae64b6ed6
Wes <wesley.landriault@gmail.com>
2015-02-03 18:10:28 -0500

    Added delete functionality.

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   get 'pictures/:id/edit' => "pictures#edit", as: "edit_picture"
   patch 'pictures/:id' => "pictures#update"
 
+  delete 'pictures/:id' => "pictures#destroy", as: "delete_picture"
 
 
   # The priority is based upon order of creation: first created -> highest priority.
