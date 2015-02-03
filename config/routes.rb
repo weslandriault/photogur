19c4b58aefc86ead446c9f846e2d724c87aa7a1b
Wes <wesley.landriault@gmail.com>
2015-02-03 16:29:44 -0500

    Introduced a form, and set create method to accept values from POST action on form.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get 'pictures' => 'pictures#index'
+
+  post 'pictures' => 'pictures#create'
+  get 'pictures/new' => 'pictures#new'
+
   get 'pictures/:id' => 'pictures#show', as: 'picture'
 
 
