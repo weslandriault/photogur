da715d4f2ac9a8ed13aad0ab55d3f197a12bb04d
Wes <wesley.landriault@gmail.com>
2015-02-04 15:34:16 -0500

    Migrated the database to add a description column, then rake:db to finalize changes.

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150204200625) do
+ActiveRecord::Schema.define(version: 20150204202844) do
 
   create_table "pictures", force: :cascade do |t|
     t.string   "artist"
     t.string   "title"
     t.string   "url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,18 +2,21 @@
 		:title  => "The old church on the coast of White sea",
 		:artist => "Sergey Ershov",
 		:url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/house.jpg"
+		:description => "You think I know what this pic is??"
 	)
 
 	Picture.create!(
 	  :title  => "Sea Power",
 	  :artist => "Stephen Scullion",
 	  :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/wave.jpg"
+	  :description => "You know this is just some pic."
 	)
 
 	Picture.create!(
 	  :title  => "Into the Poppies",
 	  :artist => "John Wilhelm",
 	  :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/girl.jpg"
+	  :description => "Man I don't know it's poppies what else do you want?!"
 	)
 
 
