8d6f19d9732b26c22bacf05669a91de1eff97cf5
Wes <wesley.landriault@gmail.com>
2015-02-04 17:51:04 -0500

    Finished assignment second time.

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -37,6 +37,13 @@ class PicturesController < ApplicationController
 		end
 	end
 
+	def destroy
+		@picture = Picture.find(params[:id])
+
+		@picture.destroy
+		redirect_to pictures_url
+	end
+
 	private
 	def picture_params
 		params.require(:picture).permit(:title, :artist, :url, :description)
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
+  # root 'pictures#index'
+  # get 'pictures' => 'pictures#index'
 
-  get 'pictures' => 'pictures#index'
+  # post 'pictures' => 'pictures#create'
+  # get 'pictures/new' => 'pictures#new'
 
-  post 'pictures' => 'pictures#create'
-  get 'pictures/new' => 'pictures#new'
+  # get 'pictures/:id/edit' => 'pictures#edit', as: "edit_picture"
+  # patch 'pictures/:id' => 'pictures#update'
 
-  get 'pictures/:id/edit' => 'pictures#edit', as: "edit_picture"
-  patch 'pictures/:id' => 'pictures#update'
+  # delete 'pictures/:id' => "pictures#destroy", as: "delete_picture"
+  # get 'pictures/:id' => 'pictures#show', as: 'picture'
 
-  get 'pictures/:id' => 'pictures#show', as: 'picture'
-
+  resources :pictures
 
 
 
